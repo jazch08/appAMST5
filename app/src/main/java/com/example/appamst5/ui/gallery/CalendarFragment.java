052271bd9e70a932b45abba0c0b269f9572d87ff
Jorge Zambrano <joraazam@espol.edu.ec>
2023-06-08 22:00:47 -0500

    Calendario con eventos

--- app/src/main/java/com/example/appamst5/ui/gallery/CalendarFragment.java
+++ app/src/main/java/com/example/appamst5/ui/gallery/CalendarFragment.java
@@ -1,9 +1,12 @@
 package com.example.appamst5.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +14,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.appamst5.databinding.FragmentCalendarBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CalendarFragment extends Fragment {
 
     private FragmentCalendarBinding binding;
+    private CalendarView calendarView;
+    private EditText editText;
+    private TextView listadoEventos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,9 +31,22 @@ public class CalendarFragment extends Fragment {
 
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        binding.btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarEventos(v);
+            }
+        });
+        //root.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        guardarEventos(v);
+        //    }
+        //});
 
-        final TextView textView = binding.textCalendar;
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        calendarView = binding.calendarView2;
+        editText = binding.editTextText2;
+        listadoEventos = binding.listEvents;
         return root;
     }
 
@@ -32,5 +54,13 @@ public class CalendarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void guardarEventos(View view) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String selectedDate = sdf.format(new Date(calendarView.getDate()));
+        Log.d("Fecha",selectedDate);
+        CharSequence addStr = editText.getText().toString() + " - " + selectedDate + "\n";
+        listadoEventos.append(addStr);
     }
 }
